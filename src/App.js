5a619cd3f90b0d2dac1b202c605bb5e0763f25fa
savio <saviopf22@gmail.com>
2020-02-04 00:23:03 -0300

    manipulando array e falta melhorar visual

--- src/App.js
+++ src/App.js
@@ -3,32 +3,63 @@ import React, { useState, useEffect } from 'react'
 function App() {
 
   const [count, setCount] = useState(-1)
+  const [numberList, setNumberList] = useState([count])
 
   useEffect(() => {
-    console.log('somou')
-  }, [count])
+    console.log('mudou o estado')
+  }, [count, numberList])
 
   return (
     <div style={{
       display: 'flex',
       flex: 1,
       alignItems: 'center',
-      flexDirection: 'column'
+      justifyContent: 'center'
     }} >
-      <h1
-        style={{
-          marginBottom: 20,
-          textAlign: 'center'
-        }}
+      <div
+        style={{ flexDirection: 'column' }}
       >
-        {count}
-      </h1>
-      <button
-        onClick={() => setCount(count + 1)}
-      >
-        Somar +1
+        <h1
+          style={{
+            marginBottom: 20,
+            textAlign: 'center'
+          }}
+        >
+          {count}
+        </h1>
+        <div style={{ flexDirection: 'row' }}>
+          {numberList.length > 1 ? (
+            numberList.map((value, index) => (
+              <h3
+                style={{ textAlign: 'center' }}
+              >
+                [{value}{index !== numberList.length - 1 ? ', ' : null}]
+              </h3>
+            ))) : (
+              <h2>[{numberList[0]}]</h2>
+            )}
+        </div>
+      </div>
+      <div style={{ flexDirection: 'row' }}>
+        <button
+          style={{ marginRight: 5 }}
+          onClick={() => setCount(count + 1)}
+        >
+          +
       </button>
-
+        <button
+          style={{ marginRight: 5 }}
+          onClick={() => setCount(count - 1)}
+        >
+          -
+      </button>
+        <button
+          style={{ marginRight: 5 }}
+          onClick={() => setNumberList([...numberList, count])}
+        >
+          [+number]
+        </button>
+      </div>
     </div>
   )
 }
